77b87fe1361a40030f24b0b3a9c866d834c54630
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-07-12 22:44:09 -0400

    Update: conditionals added

--- src/com/mortgagecalculator/mortgagecalculator.java
+++ src/com/mortgagecalculator/mortgagecalculator.java
@@ -9,29 +9,53 @@ public class mortgagecalculator {
     final byte MONTHS = 12;
     final byte PERCENTAGE = 100;
 
-    Scanner principalScanner = new Scanner(System.in);
-    System.out.print("Principal: ");
-    String principal = principalScanner.nextLine().trim();
-    int principalInt = Integer.parseInt(principal);
-    System.out.println("Entry: " + principalInt);
+    int principal = 0;
+    float annualInterest = 0;
+    float monthlyInterest = 0;
+    int period = 0;
+    int periodMonths = 0;
 
-    Scanner interestScanner = new Scanner(System.in);
-    System.out.print("Annual Interest Rate: ");
-    String interest = interestScanner.nextLine().trim();
-    double interestDble = Double.parseDouble(interest);
-    System.out.println("Entry: " + interestDble);
-    double rate = (interestDble / PERCENTAGE) / MONTHS;
+    Scanner scanner = new Scanner(System.in);
 
-    Scanner periodScanner = new Scanner(System.in);
+    while (true) {
+      System.out.print("Principal ($1k - $1M): ");
+      principal = scanner.nextInt();
+      if ((principal >= 1000) && (principal <= 1_000_000)) {
+        break;
+    }
+      System.out.println("Please enter number between 1000 and 1,000,000");
+    }
+    System.out.println("Entry: " + principal);
+
+     
+
+    while (true) {
+      System.out.print("Annual Interest Rate: ");
+      annualInterest = scanner.nextFloat();
+      if ((annualInterest >= 1) && (annualInterest <= 30)) {
+        monthlyInterest = (annualInterest/ PERCENTAGE) / MONTHS;
+        break;
+      }
+      System.out.println("Please enter a value between 1 and 30");
+    
+  }
+    System.out.println("Entry: " + annualInterest);
+    
+
+    while (true) {
     System.out.print("Period (Years): ");
-    String periodEntry = periodScanner.nextLine().trim();
-    byte periodInt = Byte.parseByte(periodEntry);
-    int period = periodInt * MONTHS;
-    System.out.println("Entry: " + periodInt);
+    period = scanner.nextInt();
+    if ((period >= 1) && (period <= 100)) {
+      periodMonths = (period * MONTHS);
+      break;
+    }
+    System.out.println("Please enter value between 1 and 100");
+  }
+    System.out.println("Entry: " + period);
 
-    double mortgage = principalInt * 
-      ((rate * Math.pow((1 + rate), period)) / 
-      ((Math.pow((1 + rate), period)) - 1));
+    double mortgage = principal * 
+      ((monthlyInterest * Math.pow((1 +monthlyInterest), periodMonths)) / 
+      ((Math.pow((1 +monthlyInterest), periodMonths)) - 1));
     NumberFormat formatter = NumberFormat.getCurrencyInstance();
     String mortgageString = formatter.format(mortgage);
     System.out.println("Mortgage: " + mortgageString);
